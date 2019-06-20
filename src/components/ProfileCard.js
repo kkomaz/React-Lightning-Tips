261d2c3eb4874ba36933e20d243e6efa5fe2a2ba
Alex Lee <alex@uniteus.com>
2019-06-20 00:34:48 -0400

    Lesson 8: Set up Tip  Form Modal

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,40 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Card,
   Button,
+  Modal,
 } from 'react-bootstrap'
 import techrally from '../assets/techrally.png'
 
 function ProfileCard() {
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => {
+    setShow(false)
+  }
+
+  const handleOpen = () => {
+    setShow(true)
+  }
+
   return (
-    <Card
-      style={{
-        background: '#52616a',
-        width: '25rem',
-        margin: '5em 0',
-      }}>
-      <Card.Img variant="top" src={techrally} />
-      <Card.Body style={{ color: 'white' }}>
-        <Card.Title>Tech Rally</Card.Title>
-        <Card.Text>
-          Welcome to the TechRally Tip page!
-        </Card.Text>
+    <React.Fragment>
+      <Card
+        style={{
+          background: '#52616a',
+          width: '25rem',
+          margin: '5em 0',
+        }}>
+        <Card.Img variant="top" src={techrally} />
+        <Card.Body style={{ color: 'white' }}>
+          <Card.Title>Tech Rally</Card.Title>
+          <Card.Text>
+            Welcome to the TechRally Tip page!
+          </Card.Text>
 
-        <Card.Text>
-          Thank you for stopping by and I hope you've been enjoying the coding videos!
-        </Card.Text>
+          <Card.Text>
+            Thank you for stopping by and I hope you've been enjoying the coding videos!
+          </Card.Text>
 
-        <Card.Text>
-          Your tip is much appreciated!
-        </Card.Text>
+          <Card.Text>
+            Your tip is much appreciated!
+          </Card.Text>
 
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <Button variant="success">
-            Tip Jar
-          </Button>
-        </div>
-      </Card.Body>
-    </Card>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Button variant="success" onClick={handleOpen}>
+              Tip Jar
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Tip Jar</Modal.Title>
+        </Modal.Header>
+          <Modal.Body>
+            Tip Form
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+      </Modal>
+    </React.Fragment>
   )
 }
 
