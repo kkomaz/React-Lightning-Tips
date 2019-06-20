e3eff7cfb18791ebcaf923cba6446f3db113750e
Alex Lee <alex@uniteus.com>
2019-06-20 01:03:06 -0400

    Lesson 10: Create Tip Part II

--- src/components/TipForm.js
+++ src/components/TipForm.js
@@ -3,6 +3,8 @@ import {
   Button,
   Form,
 } from 'react-bootstrap'
+import axios from 'axios'
+import keys from '../config/keys'
 
 function TipForm(props) {
   const handleSubmit = async (e) => {
@@ -13,20 +15,39 @@ function TipForm(props) {
       formAmount,
     } = e.currentTarget
 
+    const params = {
+      amount: formAmount.value,
+      description: 'Tip to Tech Rally',
+      currency: 'USD'
+    }
+
+    try {
+      const result = await axios.post(`/charges`, params, {
+        headers: { Authorization: keys.openNodeKey }
+      })
+
+      if (result) {
+        const charge = result.data.data
+        window.open(`${keys.openNodeCheckoutUrl}/${charge.id}`)
+      }
+    } catch (e) {
+      alert(e.message)
+    }
+
     formAmount.value = ''
   }
 
   return (
     <Form onSubmit={e => handleSubmit(e)}>
       <Form.Group controlId="formAmount">
-        <Form.Label>Contribution Amount (in USD)</Form.Label>
+        <Form.Label>Tip Amount (in USD)</Form.Label>
         <Form.Control
           required
           type="number"
-          placeholder="Enter Contribution"
+          placeholder="Enter Tip amount (USD)"
           step="0.01"
         />
-      <Form.Control.Feedback type="invalid">Please enter a description</Form.Control.Feedback>
+      <Form.Control.Feedback type="invalid">Please enter a tip amount!</Form.Control.Feedback>
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
